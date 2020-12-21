eb884c92d05b6ae02a8dfe803b3d8ee5f4ecdfa9
Guilherme Marmerola <guilherme.marmerola@loft.com.br>
2020-12-21 00:36:16 -0300

    minor changes

--- app/app_portuguese.py
+++ app/app_portuguese.py
@@ -60,7 +60,7 @@ def load_data():
 st.title('Qual é a raça do meu dog?')
 st.write(
     'Esse aplicativo usa os dados do [Stanford Dogs Dataset](http://vision.stanford.edu/aditya86/ImageNetDogs/) '
-    'e *deep learning* para identificar a raça de cães usando imagens. '
+    'e *deep learning* para identificar a raça do seu dog usando imagens. '
     "É só enviar a foto do seu amigo e o algoritmo vai buscar 50 "
     'cães comparáveis na base, retornando as raças deles. '
     'Mais detalhes sobre a metodologia [neste artigo](https://gdmarmerola.github.io/discovering-breed-with-ml/).'
